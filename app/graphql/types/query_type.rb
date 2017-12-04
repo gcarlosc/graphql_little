bd3b5b7a0719472d117135c7a3fb245333b635f4
giancarlo <gsoliscastillo@gmail.com>
2017-12-04 11:15:37 -0300

    add current user context

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -12,11 +12,11 @@ Types::QueryType = GraphQL::ObjectType.define do
 
   field :me, Types::UserType do
     description 'current user'
-    resolve ->(obj, args, ctx) { User.first }
+    resolve ->(obj, args, ctx) { ctx[:current_user] }
   end
 
   field :projects, types[Types::ProjectType] do
     description 'projects of the current user'
-    resolve ->(obj, args, ctx) { Project.all }
+    resolve ->(obj, args, ctx) { ctx[:current_user].projects }
   end
 end
